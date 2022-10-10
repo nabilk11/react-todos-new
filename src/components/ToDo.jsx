1fc132286d88cbc1e91669fea8d0046d9e4a9cb7
nabilk11 <khan.nabiln11.com>
2022-10-09 21:51:05 -0400

    added strikethrough styling for completed tasks, with conditional classname

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 export const ToDo = ({ todo }) => {
   return (
     <div className='card'>
-    <h1>Task: {todo.task}</h1>
-    {todo.complete ? <p className='complete-emo'>✅</p> : <p className='inc-emo'>⛔️</p> }
+    <div>
+    <h3 className={todo.complete ? 'complete' : 'incomplete'}>{todo.task}</h3>
+    {todo.complete ? <span className='complete-emo'>✅</span> : <span className='inc-emo'>⛔️</span> }
+
+    </div>
           
-    
-    
     </div>
   )
 }
