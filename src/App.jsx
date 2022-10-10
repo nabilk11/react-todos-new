c73ccdf787a3c27e9f061c272b0bc95eb8c2fb9b
nabilk11 <khan.nabiln11.com>
2022-10-09 21:02:41 -0400

    added readme, updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { Header } from './components/Header'
+import { ToDoList } from './components/ToDoList'
+import data from './data/data.json'
+
+
 
 function App() {
+
+  // vite app counter
   const [count, setCount] = useState(0)
 
+
+  // todolist state
+  const [toDoList, setToDoList] = useState(data)
+  
+  
+  
+  
+  
   return (
     <div className="App">
       <Header />
+      <ToDoList toDoList={toDoList}/>
       <div className="card">
 
-        <button onClick={() => setCount((banana) => count + 1)}>
+        <button onClick={() => setCount((counter) => count + 1)}>
           count is {count}
         </button> 
         
